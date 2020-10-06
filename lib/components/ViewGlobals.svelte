4df8342e201cf0d11785748e8ac8267314e6c4e1
Maximiliano Ruani <mruani@inboxlab.com>
2020-10-06 13:43:45 -0300

    - Work in progress

---
+++ lib/components/ViewGlobals.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { setContext } from 'svelte';
+    import { writable } from 'svelte/store';
+
+    export let component;
+    export let componentProps = {};
+
+    export let globalAssets = {};
+    export let globalProps = {};
+    export let globalStore = {};
+
+    globalAssets.host = globalAssets.host || '';
+    globalAssets.scripts = globalAssets.scripts || new Map();
+    globalAssets.styles = globalAssets.styles || new Map();
+
+    setContext('global.assets', globalAssets);
+    setContext('global.props', globalProps);
+    setContext('global.store', writable(globalStore));
+</script>
+
+<svelte:component this={component} {...componentProps} />
