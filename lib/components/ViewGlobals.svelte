f73b039e52b767c8cb3e2db084d10a3c11301e42
Maximiliano Ruani <mruani@inboxlab.com>
2021-04-29 13:02:23 -0300

    - Renamed globalStore to globalStores and make every entry a store

--- lib/components/ViewGlobals.svelte
+++ lib/components/ViewGlobals.svelte
@@ -7,15 +7,25 @@
 
     export let globalAssets = {};
     export let globalProps = {};
-    export let globalStore = {};
+    export let globalStores = {};
 
     globalAssets.host = globalAssets.host || '';
     globalAssets.scripts = globalAssets.scripts || new Map();
     globalAssets.styles = globalAssets.styles || new Map();
 
+    function _createStores(props) {
+        const entries = Object.entries(props);
+        const output = {};
+        for (let i = 0; i < entries.length; i++) {
+            const [key, value] = entries[i];
+            output[key] = writable(value);
+        }
+        return output;
+    }
+
     setContext('global.assets', globalAssets);
     setContext('global.props', globalProps);
-    setContext('global.store', process.browser === false ? writable(globalStore) : globalStore);
+    setContext('global.stores', process.browser === false ? _createStores(globalStores) : globalStores);
 </script>
 
 <svelte:component this={component} {...props} />
